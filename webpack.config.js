e61c1562eb6e1e93e2d6d79cac755752a1aab1fd
Stanley Wang <ccwangdev@gmail.com>
2019-07-20 06:46:15 +0000

    Setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require("path");
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (process.env.NODE_ENV === "test") {
+    require("dotenv").config({ path: ".env.test" });
+} else if (process.env.NODE_ENV === "development") {
+    require("dotenv").config({ path: ".env.development" });
+}
 
 const webpackConfig = (env) => {
     const isProduction = env === "production";
@@ -47,6 +56,15 @@ const webpackConfig = (env) => {
         plugins:[
             new MiniCssExtractPlugin({
                 filename: "styles.css"
+            }),
+            new webpack.DefinePlugin({
+                "process.env.FIREBASE_API_KEY": Json.stringfy(process.env.FIREBASE_API_KEY),
+                "process.env.FIREBASE_AUTH_DOMAIN": Json.stringfy(process.env.FIREBASE_AUTH_DOMAIN),
+                "process.env.FIREBASE_DATABASE_URL": Json.stringfy(process.env.FIREBASE_DATABASE_URL),
+                "process.env.FIREBASE_PROJECT_ID": Json.stringfy(process.env.FIREBASE_PROJECT_ID),
+                "process.env.FIREBASE_STORAGE_BUCKET": Json.stringfy(process.env.FIREBASE_STORAGE_BUCKET),
+                "process.env.FIREBASE_MESSAGING_SENDER_ID": Json.stringfy(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                "process.env.FIREBASE_APP_ID": Json.stringfy(process.env.FIREBASE_APP_ID)
             })
         ],
     };
