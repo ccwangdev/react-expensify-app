4fe2c56230d24ef68d6cc4479b7be8734a4ebebd
Stanley Wang <ccwangdev@gmail.com>
2019-07-20 06:49:54 +0000

    Update JSON on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -58,13 +58,13 @@ const webpackConfig = (env) => {
                 filename: "styles.css"
             }),
             new webpack.DefinePlugin({
-                "process.env.FIREBASE_API_KEY": Json.stringfy(process.env.FIREBASE_API_KEY),
-                "process.env.FIREBASE_AUTH_DOMAIN": Json.stringfy(process.env.FIREBASE_AUTH_DOMAIN),
-                "process.env.FIREBASE_DATABASE_URL": Json.stringfy(process.env.FIREBASE_DATABASE_URL),
-                "process.env.FIREBASE_PROJECT_ID": Json.stringfy(process.env.FIREBASE_PROJECT_ID),
-                "process.env.FIREBASE_STORAGE_BUCKET": Json.stringfy(process.env.FIREBASE_STORAGE_BUCKET),
-                "process.env.FIREBASE_MESSAGING_SENDER_ID": Json.stringfy(process.env.FIREBASE_MESSAGING_SENDER_ID),
-                "process.env.FIREBASE_APP_ID": Json.stringfy(process.env.FIREBASE_APP_ID)
+                "process.env.FIREBASE_API_KEY": JSON.stringfy(process.env.FIREBASE_API_KEY),
+                "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringfy(process.env.FIREBASE_AUTH_DOMAIN),
+                "process.env.FIREBASE_DATABASE_URL": JSON.stringfy(process.env.FIREBASE_DATABASE_URL),
+                "process.env.FIREBASE_PROJECT_ID": JSON.stringfy(process.env.FIREBASE_PROJECT_ID),
+                "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringfy(process.env.FIREBASE_STORAGE_BUCKET),
+                "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringfy(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                "process.env.FIREBASE_APP_ID": JSON.stringfy(process.env.FIREBASE_APP_ID)
             })
         ],
     };
