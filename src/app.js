24688954e3982d05a6b5c6c1877a3cf544420390
Stanley Wang <ccwangdev@gmail.com>
2019-07-21 00:45:19 +0000

    Add private routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux"
-import AppRouter from "./routes/AppRouter";
+import AppRouter, { history } from "./routes/AppRouter";
 import configureStore from "./store/configureStore";
 import selectExpenses from "./selectors/expenses";
 import { startSetExpenses } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { login, logout } from "./actions/auth";
 import "normalize.css/normalize.css";
 import "./styles/style.scss";
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
-import "./firebase/firebase";
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
 
@@ -20,8 +20,29 @@ const jsx = (
     </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById("app"));
+        hasRendered = true;
+    } 
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById("app"));
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === "/") {
+                history.push("/dashboard");
+            } 
+        });
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push("/");
+    }
 });
