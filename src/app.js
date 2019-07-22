0bb88265ab3c13edc5b3771394353432e42e2a3c
Stanley Wang <ccwangdev@gmail.com>
2019-07-22 02:44:48 +0000

    Add Expensity styles

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@ import selectExpenses from "./selectors/expenses";
 import { startSetExpenses } from "./actions/expenses";
 import { login, logout } from "./actions/auth";
 import "normalize.css/normalize.css";
-import "./styles/style.scss";
+import "./styles/styles.scss";
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -28,8 +29,7 @@ const renderApp = () => {
     } 
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
-
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
